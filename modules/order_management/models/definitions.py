56555fa6b41a76bd32bc8c36a4e1a774f79bcd15
Arthur Testa <arthurtesta@gmail.com>
2022-12-28 11:58:57 -0300

    changes to definitions / models

--- modules/order_management/models/definitions.py
+++ modules/order_management/models/definitions.py
@@ -4,33 +4,32 @@ from django.db import models
 from enum import Enum, unique
 
 from modules.core.models.definitions import Transaction
+from ..utils import Validators
 
 # Create your models here.
 
 """Objective business-related entities / models"""
 
 
-@unique
-class OrderKind(str, Enum):
-    """Types of shipping order"""
-
-    LTL = "LTL"
-    FTL = "FTL"
-
-    @classmethod
-    def choices(cls):
-        return tuple((i.value, (i.name.capitalize())) for i in cls)
-
-
 class ShippingOrder(Transaction):
     """A shipping order is a statement of intent, between a seller/buyer
      and a logistic provider, to move a one or more items. It contains many purchase orders """
+    # TODO add organization selector
 
-    mode = OrderKind.choices()
+    TRANSPORT_TYPE = (
+        ('TL', 'Truck Load'),
+        ('LTL', 'Less than Truck Load'),
+        ('PTL', 'Partial Truck Load'),
+        ('RAIL', 'Rail'),
+        ('DRAYAGE', 'Drayage')
+    )
+
+    mode = models.CharField(choices=TRANSPORT_TYPE, null=True, default=None, max_length=7)
     date_received = models.DateTimeField(auto_now_add=True)
     last_update = models.DateTimeField(auto_now_add=True)
     pickup_date = models.DateTimeField(auto_now_add=False)
     delivery_date = models.DateTimeField(auto_now_add=False)
+    reference = models.CharField(max_length=128, default='')
 
 
 class PurchaseOrder(Transaction):
@@ -38,7 +37,8 @@ class PurchaseOrder(Transaction):
      and a seller, for exchanged goods and services. It may belong to
      one or zero ShippingOrder. It may contain one or many Items """
 
-    shipping_order_id = models.ForeignKey(ShippingOrder, related_name="+", on_delete=models.SET("N/A"))
+    shipping_order_id = models.ForeignKey(ShippingOrder, on_delete=models.SET("N/A"))
+
     # TODO add params from serializer
 
     @property
@@ -48,6 +48,9 @@ class PurchaseOrder(Transaction):
             total += item.total
         return total
 
+
+# maybe Item should be an abstract definition for the general concept
+# of an Item, and here we should have a OneToOne "ItemInstance" class with it's own ID
 
 class Item(models.Model):
     """An Item represents a package of a specific commodity to be transported within
@@ -73,13 +76,13 @@ class Item(models.Model):
 
     purchase_order_id = models.ForeignKey(PurchaseOrder, related_name="items", on_delete=models.SET("N/A"))
     commodity = models.CharField(max_length=128)
-    count = models.IntegerField # TODO add params on these
+    count = models.CharField(validators=[Validators.zero_not_first_integer], max_length=7, default=0)
     package_type = models.CharField(max_length=2, choices=PACKAGE_TYPE)
-    weight = models.IntegerField
+    weight = models.IntegerField(validators=[Validators.validate_comma_separated_integer], default=0)
     weight_unit = models.CharField(max_length=2, choices=WEIGHT_UNIT)
-    height = models.IntegerField
-    width = models.IntegerField
-    length = models.IntegerField
+    height = models.IntegerField(validators=[Validators.zero_not_first_integer], default=0)
+    width = models.IntegerField(validators=[Validators.zero_not_first_integer], default=0)
+    length = models.IntegerField(validators=[Validators.zero_not_first_integer], default=0)
     dimension_unit = models.CharField(max_length=2, choices=DIMENSION_UNIT)
 
     def __str__(self):
---
+++ modules/order_management/utils.py
@@ -0,0 +1,19 @@
+from django.core.exceptions import ValidationError
+from django.utils.translation import gettext_lazy as _
+
+
+class Validators:
+    def validate_comma_separated_integer(value):
+        if value.find('.') != 4:
+            raise ValidationError(
+                _('%(value)s decimal point incorrect (e.g. 1234.56)'),
+                params={'value': value},
+            )
+
+    def zero_not_first_integer(value):
+        form_input = value
+        if form_input[0] == 0:
+            raise ValidationError(
+                _('%(value)s first integer must not be zero'),
+                params={'value': value}
+            )
