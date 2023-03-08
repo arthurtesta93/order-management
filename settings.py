603cff5d3fb3228d0bc90c0db0b16ad22ff1631c
Arthur Testa <arthurtesta@gmail.com>
2023-03-08 14:27:41 -0300

    add cors headers

--- settings.py
+++ settings.py
@@ -47,7 +47,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    "corsheaders",
+    'corsheaders',
     'modules.core',
     'modules.order_management'
 ]
